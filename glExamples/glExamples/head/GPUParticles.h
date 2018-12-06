187ff0669f32d17de9983335a071907c0f6c85e7
DESKTOP-ABBNTO3\yangtao <1359870821@qq.com>
2018-12-06 23:33:49 +0800

    no message

--- glExamples/glExamples/head/GPUParticles.h
+++ glExamples/glExamples/head/GPUParticles.h
@@ -2,9 +2,21 @@
 
 #include "BaseClass.h"
 
+static const int ParticlesWidth = 100;
+
 class gpuParticles : public baseClass::application {
 	void start() {
-
+		GLfloat particlesData[ParticlesWidth * ParticlesWidth * 4];
+		for (size_t i = 0; i < ParticlesWidth; i++)
+		{
+			for (size_t j = 0; j < ParticlesWidth; j++)
+			{
+				particlesData[j * 4 + 0 + ParticlesWidth * i * 4] = 0.0f;
+				particlesData[j * 4 + 1 + ParticlesWidth * i * 4] = 0.0f;
+				particlesData[j * 4 + 2 + ParticlesWidth * i * 4] = 0.0f;
+				particlesData[j * 4 + 3 + ParticlesWidth * i * 4] = -1.0f;
+			}
+		}
 	}
 
 	void render(double time) {
@@ -17,4 +29,8 @@ private:
 	Shader updateShader;
 
 	GLuint particleTexture;
+
+	GLuint updateFrameBuffers[2];
+	GLuint updateTextures[2];
+	GLuint particleVertexPositionVBO;
 };
