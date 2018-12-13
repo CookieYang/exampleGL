90054a89c6e3d42477a2897c8ea615efad5bcf77
yangtao <1359870821@qq.com>
2018-12-13 11:49:47 +0800

    shadowMap 2

--- glExamples/glExamples/head/shadowMap.h
+++ glExamples/glExamples/head/shadowMap.h
@@ -1,25 +1,52 @@
 #pragma once
 #include "BaseClass.h"
+#include "KTXLoader.h"
 
 class ShadowMap : public baseClass::application {
 public:
 	void start() {
-		shadowMapShader = Shader("E://ogl/glExamples/glExamples/shaders/shadowMap/shadowMap.vs", "E://ogl/glExamples/glExamples/shaders/shadowMap/shadowMap.fs");
-		sceneShader = Shader("E://ogl/glExamples/glExamples/shaders/shadowMap/scene.vs", "E://ogl/glExamples/glExamples/shaders/shadowMap/scene.fs");
+		shadowMapShader = Shader("D://ogl/glExamples/glExamples/shaders/shadowMap/shadowMap.vs", "D://ogl/glExamples/glExamples/shaders/shadowMap/shadowMap.fs");
+		sceneShader = Shader("D://ogl/glExamples/glExamples/shaders/shadowMap/scene.vs", "D://ogl/glExamples/glExamples/shaders/shadowMap/scene.fs");
 
 		shadowMapUniform.model_matrix = glGetUniformLocation(shadowMapShader.Program, "model_matrix");
 		shadowMapUniform.view_matrix = glGetUniformLocation(shadowMapShader.Program, "view_matrix");
 		shadowMapUniform.proj_matrix = glGetUniformLocation(shadowMapShader.Program, "proj_matrix");
 		shadowMapUniform.obj_color = glGetUniformLocation(shadowMapShader.Program, "obj_color");
 
-		torus.load("E://ogl/media/torus_nrms_tc.sbm");
-		cube.load("E://ogl/media/cube.sbm");
+		sceneUniform.model_matrix = glGetUniformLocation(sceneShader.Program, "model_matrix");
+		sceneUniform.view_matrix = glGetUniformLocation(sceneShader.Program, "view_matrix");
+		sceneUniform.proj_matrix = glGetUniformLocation(sceneShader.Program, "proj_matrix");
+
+		torus.load("D://ogl/media/torus_nrms_tc.sbm");
+		cube.load("D://ogl/media/cube.sbm");
 
 		glEnable(GL_DEPTH_TEST);
+		//glDepthFunc(GL_GREATER);
+
+		glGenFramebuffers(1, &shadowFBO);
+		glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
+
+		testTexture = ktx::load("D://ogl/media/baboon.ktx");
+
+		glGenTextures(1, &shadowTexture);
+		glBindTexture(GL_TEXTURE_2D, shadowTexture);
+		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT32F, 800, 600);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, shadowTexture, 0);
+
+		glDrawBuffer(GL_NONE);
+		//glReadBuffer(GL_NONE);
 	}
 
 	void render(double time) {
-		static const GLfloat gray[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+		static const GLfloat gray[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 		static const GLfloat red[] = {1.0f, 0.2f, 0.2f, 1.0f };
 		static const GLfloat green[] = { 0.2f, 1.0f, 0.2f, 1.0f };
 		static const GLfloat ones[] = { 1.0f };
@@ -35,6 +62,7 @@ public:
 
 		glClearBufferfv(GL_COLOR, 0, gray);
 		glClearBufferfv(GL_DEPTH, 0, ones);
+		glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
 		shadowMapShader.Use();
 		glUniformMatrix4fv(shadowMapUniform.model_matrix, 1, GL_FALSE, cube_matrix);
 		glUniformMatrix4fv(shadowMapUniform.view_matrix, 1, GL_FALSE, view_matrix);
@@ -46,6 +74,25 @@ public:
 		glUniformMatrix4fv(shadowMapUniform.model_matrix, 1, GL_FALSE, torus_matrix);
 		glUniform4fv(shadowMapUniform.obj_color, 1, green);
 		torus.render();
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glDrawBuffer(GL_BACK);
+		glViewport(0, 0, 800.0, 600.0);
+		glClearBufferfv(GL_COLOR, 0, gray);
+		glClearBufferfv(GL_DEPTH, 0, gray);
+		glDisable(GL_DEPTH_TEST);
+		view_position = vmath::vec3(0.0f, 0.0f, 50.0f);
+		view_matrix = vmath::lookat(view_position,
+			vmath::vec3(0.0f, 0.0f, 0.0f),
+			vmath::vec3(0.0f, 1.0f, 0.0f));
+		sceneShader.Use();
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, shadowTexture);
+		glUniform1i(glGetUniformLocation(sceneShader.Program, "depthTex"), 0);
+		//glUniformMatrix4fv(sceneUniform.model_matrix, 1, GL_FALSE, cube_matrix);
+		//glUniformMatrix4fv(sceneUniform.view_matrix, 1, GL_FALSE, view_matrix);
+		//glUniformMatrix4fv(sceneUniform.proj_matrix, 1, GL_FALSE, proj_matrix);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 	}
 
 private:
@@ -57,8 +104,18 @@ private:
 		GLuint proj_matrix;
 		GLuint obj_color;
 	}shadowMapUniform;
+
+	struct
+	{
+		GLuint model_matrix;
+		GLuint view_matrix;
+		GLuint proj_matrix;
+	}sceneUniform;
+
 	Shader sceneShader;
 	GLuint shadowTexture;
+	GLuint testTexture;
+	GLuint shadowFBO;
 	baseClass::object torus;
 	baseClass::object cube;
 };
